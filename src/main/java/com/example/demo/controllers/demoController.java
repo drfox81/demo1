95b1790cfa23c31388d78fb5126e554b628f4ede
Drfox81 <Drfox11181@mail.ru>
2023-01-05 16:53:14 +0300

    12

--- src/main/java/com/example/demo/controllers/demoController.java
+++ src/main/java/com/example/demo/controllers/demoController.java
@@ -1,9 +1,25 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class demoController {
+
+    @GetMapping
+    public String firstMethod(){
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String secondMethod(){
+        LocalDate date=LocalDate.of(2023,1,5);
+        return "Ученик - Денис Кокорин || " +"Рецепты от Дениса Кокорина || "+
+                date+" || собраны лучшие рецепты блюд, которые с легкостью приготовит " +
+                "даже подросток";
+    }
 
 
 }
